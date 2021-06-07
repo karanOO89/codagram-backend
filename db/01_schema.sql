3e4ece588834be230e34f751162198f75137d334
Harp Sandhu <happacode@protonmail.com>
2021-06-07 11:56:02 -0700

    Add schema to pg db

--- db/01_schema.sql
+++ db/01_schema.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS user CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS user_followers CASCADE;
 DROP TABLE IF EXISTS posts CASCADE;
 DROP TABLE IF EXISTS post_likes CASCADE;
-DROP TABLE IF EXISTS posts_comments CASCADE;
+DROP TABLE IF EXISTS post_comments CASCADE;
 DROP TABLE IF EXISTS dm_rooms CASCADE;
 DROP TABLE IF EXISTS messages CASCADE;
 
@@ -14,12 +14,12 @@ CREATE TABLE users (
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
   created TIMESTAMP,
-  avatar_url VARCHAR(255) NOT NULL,
+  avatar_url VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE posts(
   id SERIAL PRIMARY KEY NOT NULL,
-  FOREIGN KEY (user_id) INTEGER REFERENCES users(id),
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   type VARCHAR(255) NOT NULL,
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
@@ -30,16 +30,16 @@ CREATE TABLE posts(
 
 CREATE TABLE post_likes (
   id SERIAL PRIMARY KEY NOT NULL,
-  FOREIGN KEY (user_id) INTEGER REFERENCES users(id),
-  FOREIGN KEY (post_id) INTEGER REFERENCES posts(id),
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
   created_at TIMESTAMP,
   updated_at TIMESTAMP
 );
 
 CREATE TABLE post_comments (
   id SERIAL PRIMARY KEY NOT NULL,
-  FOREIGN KEY (user_id) INTEGER REFERENCES users(id),
-  FOREIGN KEY (post_id) INTEGER REFERENCES posts(id),
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
   comments VARCHAR(255) NOT NULL,
   created_at TIMESTAMP,
   updated_at TIMESTAMP
@@ -48,29 +48,29 @@ CREATE TABLE post_comments (
 
 CREATE TABLE user_followers (
   id SERIAL PRIMARY KEY NOT NULL,
-  FOREIGN KEY (user_id) INTEGER REFERENCES users(id),
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   follower_id INTEGER,
   updated_at TIMESTAMP,
   created TIMESTAMP
 );
 
+CREATE TABLE dm_rooms (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user1_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  user2_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP
+);
 
 CREATE TABLE messages (
   id SERIAL PRIMARY KEY NOT NULL,
-  FOREIGN KEY (user_id) INTEGER REFERENCES users(id),
-  FOREIGN KEY (room_id) INTEGER REFERENCES dm_rooms(id),
-  message VARCHAR(255) NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  dm_room_id INTEGER REFERENCES dm_rooms(id) ON DELETE CASCADE,
+  msg VARCHAR(255) NOT NULL,
   created_at TIMESTAMP,
   updated_at TIMESTAMP
 );
 
-CREATE TABLE dm_rooms (
-  id SERIAL PRIMARY KEY NOT NULL,
-  FOREIGN KEY (user1_id) INTEGER REFERENCES users(id),
-  FOREIGN KEY (user2_id) INTEGER REFERENCES users(id),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
-);
 
 
 
--- db/index.js
+++ db/index.js
@@ -1,7 +1,13 @@
-const { Pool } = require('pg')
-const pool = new Pool()
-module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback)
-  },
-}
+const pg = require("pg");
+
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL || 
+  `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@localhost:8080/${process.env.PGDATABASE}`,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+});
+
+client
+  .connect()
+  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+
+module.exports = client;
